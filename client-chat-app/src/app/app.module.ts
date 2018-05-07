492d06173d49e53d736a7cd4ff76b62b8d3a03f1
shinson <sonia.hinson@gmail.com>
2018-05-06 21:26:30 -0400

    initial generated code for client application

---
+++ client-chat-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { MessageComponent } from './message/message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    MessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
